b45abdbf4821eb0ef93c6c0399f408192de1fc9d
ZubYar <zubyar@mail.ru>
2025-02-28 13:19:44 +0300

    Написал свой опросник

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,24 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            const string myName = "Yaroslav";
-            byte myAge = 27;
-            bool myPet = true;
-            float myShoesSize = 42;
-            Console.WriteLine("Меня зовут " + myName);
-            Console.WriteLine("Мне " + myAge + " лет");
-            Console.WriteLine("Есть ли у меня питомец? " + myPet);
-            Console.WriteLine("Мой размер ноги " + myShoesSize);
-            Console.ReadKey();
-        }
+        static void Main(string[] args);
     }
-}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your age? ");
+        var age = checked ((byte)int.Parse(Console.ReadLine()));
+        Console.Write("What is your birthdate? ");
+        var birthdate = Console.ReadLine();
+        Console.Write($"Nice to meet you, {name}. I know you age is {age} and your birhdate is {birthdate}");
+
+
+    }
+}
